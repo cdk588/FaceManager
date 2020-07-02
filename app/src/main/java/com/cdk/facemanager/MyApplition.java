c6f754b0eba7ca03663cd1c7bcaba04718a9e6e6
cdk <1092682989@qq.com>
2020-07-02 11:12:33 +0800

    第一次

---
+++ app/src/main/java/com/cdk/facemanager/MyApplition.java
@@ -0,0 +1,10 @@
+package com.cdk.facemanager;
+
+import android.app.Application;
+
+public class MyApplition extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
